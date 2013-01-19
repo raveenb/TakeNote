3426ac2d69d01ef72f51ad8bc80d822f6af54099
Raveen Beemsingh <raveen.b@gmail.com>
2013-01-19 00:58:16 -0500

    dumped dot cloud, moved to heroku

--- takenotex/takenotex/settings.py
+++ takenotex/takenotex/settings.py
@@ -1,12 +1,4 @@
-# Django settings for minestrone project.
-
-import os
-import json
-import djcelery
-
-# Load the DotCloud environment
-with open('/home/dotcloud/environment.json') as f:
-  dotcloud_env = json.load(f)
+# Django settings for takenotex project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -19,41 +11,19 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'template1',
-        'USER': dotcloud_env['DOTCLOUD_DB_SQL_LOGIN'],
-        'PASSWORD': dotcloud_env['DOTCLOUD_DB_SQL_PASSWORD'],
-        'HOST': dotcloud_env['DOTCLOUD_DB_SQL_HOST'],
-        'PORT': int(dotcloud_env['DOTCLOUD_DB_SQL_PORT']),
-    }
-}
-
-# Configure Celery using the RabbitMQ credentials found in the DotCloud
-# environment.
-djcelery.setup_loader()
-BROKER_HOST = dotcloud_env['DOTCLOUD_BROKER_AMQP_HOST']
-BROKER_PORT = int(dotcloud_env['DOTCLOUD_BROKER_AMQP_PORT'])
-BROKER_USER = dotcloud_env['DOTCLOUD_BROKER_AMQP_LOGIN']
-BROKER_PASSWORD = dotcloud_env['DOTCLOUD_BROKER_AMQP_PASSWORD']
-BROKER_VHOST = '/'
-
-# A very simple queue, just to illustrate the principle of routing.
-CELERY_DEFAULT_QUEUE = 'default'
-CELERY_QUEUES = {
-    'default': {
-        'exchange': 'default',
-        'exchange_type': 'topic',
-        'binding_key': 'tasks.#'
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+# In a Windows environment this must be set to your system time zone.
 TIME_ZONE = 'America/Chicago'
 
 # Language code for this installation. All choices can be found here:
@@ -67,32 +37,30 @@ SITE_ID = 1
 USE_I18N = True
 
 # If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
+# calendars according to the current locale.
 USE_L10N = True
+
+# If you set this to False, Django will not use timezone-aware datetimes.
+USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/home/dotcloud/data/media/'
+MEDIA_ROOT = ''
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = '/media/'
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dotcloud/volatile/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
@@ -110,7 +78,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '7bbxv&^4%f^en*c_c9*y+jrlfp+lwym$4b6((ok4v&8h9g%rwx'
+SECRET_KEY = '$kq^fsv*)4ll@xhrb1f4emigkay16v@7-fqtvtwu=*a67pk134'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -125,15 +93,19 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'minestrone.urls'
+ROOT_URLCONF = 'takenotex.urls'
+
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'takenotex.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'templates')
 )
 
 INSTALLED_APPS = (
@@ -144,24 +116,28 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'minestrone.soup',
-    'djcelery',
 )
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
+# the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
         }
     },
